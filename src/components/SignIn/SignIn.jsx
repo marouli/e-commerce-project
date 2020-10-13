91c09a9ab2c99a26ea3700aca27a9ce0bda4db07
Maria Sidiropoulou <maroulisid@gmail.com>
2020-10-13 16:48:26 +0200

    sign in with email

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../FormInput/FormInput';
 import CustomButton from '../CustomButton/CustomButton';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './SignIn.scss';
 
 class SignIn extends React.Component {
@@ -14,10 +14,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
--- src/pages/SignInSignUpPage/SignInSignUpPage.jsx
+++ src/pages/SignInSignUpPage/SignInSignUpPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 import './SignInSignUpPage.scss';
 
 const SignInSignUpPage = () => (
   <div className='sign-in-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
