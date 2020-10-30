ba06378c0b53f1e0e9813ff0893ffa64d49c7381
Maria Sidiropoulou <maroulisid@gmail.com>
2020-10-30 15:13:51 +0100

    getting collections from firebase and updating shop page redux store

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
   measurementId: 'G-7VDZFYJBGH',
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -54,7 +56,23 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
-firebase.initializeApp(config);
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
