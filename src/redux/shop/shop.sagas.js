6027ef4cd33db547729b9a1218fa71b853bc3ec2
Maria Sidiropoulou <maroulisid@gmail.com>
2020-12-10 16:50:33 +0100

    clear cart saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
 import {
   firestore,
@@ -28,4 +28,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
